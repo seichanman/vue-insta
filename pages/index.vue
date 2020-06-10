921df40522137163bf1077282cee49f40bb4a5a9
seishirou <sei.gakkusyuu@gmail.com>
2020-06-10 17:03:44 +0900

    header・home作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,61 @@
 <template>
-  <div class="container">
-    <h1 class="title">
-      sns-for-movie
-    </h1>
+  <div>
+    <header class="header">
+      <div class="header__container">
+        <div class="header__logo">
+          <img src="/images/logo.png" class="">
+        </div>
+        <img class="header__post" src="/images/letter.svg">
+      </div>
+    </header>
+    <div class="container">
+      <posts />
+    </div>
   </div>
 </template>
 
 <script>
+
+import Posts from '~/components/Posts.vue'
 export default {
-  components: {}
-};
+ components: {
+   Posts
+ }
+}
 </script>
 
 <style lang="scss" scoped>
-.container {
-  h1 {
-    color: $base-1;
-    @include sm {
-      color: $gray-2;
+  .header{
+    border: 1px solid #ddd;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 999;
+    background: #fff;
+    &__container{
+      position: relative;
+      max-width: $container;
+      margin: 0 auto;
+      padding:10px 20px;
+    }
+    &__logo{
+      margin: 0 auto;
+      max-width: 80px;
+      img{
+        display: flex;
+        align-items: center;
+      }
+    }
+    &__post{
+      position: absolute;
+      top: 50%;
+      right: 20px;
+      transform: translateY(-50%);
+      max-width: 24px;
     }
   }
-}
+  .container{
+    margin:50px 0 52px;
+  }
 </style>
